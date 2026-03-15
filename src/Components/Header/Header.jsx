cb3525aaa049f78c05287afab82a5dcd611da438
Alexander Artemiev <alexanderma1983@gmail.com>
2026-03-15 16:31:49 -0500

    Actualizacion presentacion del portafolio

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Header.css';
 
 export function Header() {
     const [openMenu, setOpenMenu] = useState(false);
+    const [openLanguage, setOpenLanguage] = useState(false);
 
     const toggleMenu = () => {
         setOpenMenu(!openMenu);
     };
+    const toggleLanguage = (e) =>{
+        e.stopPropagation();
+        setOpenLanguage(!openLanguage);
+    }
+    const selectLanguage = () => {
+        setOpenLanguage(false);
+        setOpenMenu(false);
+    }
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.target.closest('.header__content')) {
+                setOpenMenu(false);
+                setOpenLanguage(false);
+            }
+        };
 
+        document.addEventListener('click', handleClickOutside);
+        return () => document.removeEventListener('click', handleClickOutside);
+    }, []);
     return(
         <>
             <div className='header__content' >
@@ -23,8 +42,21 @@ export function Header() {
                     <button >Inicio</button>
                     <button >Servicios</button>
                     <button >Habilidades</button>
-                    <button >Experiencia</button>
                     <button >Sobre mí</button>
+                    <button >Contacto</button>
+                    <div className='language__content'>
+                        <button 
+                            className='language__content_button'
+                            onClick={toggleLanguage}>
+                            Idioma
+                        </button>
+                        <div className={`language__submenu ${openLanguage ? 'show' : ''}`}>
+                            <button onClick={() => selectLanguage('es')}>🇪🇸 Español</button>
+                            <button onClick={() => selectLanguage('en')}>🇬🇧 Inglés</button>
+                            <button onClick={() => selectLanguage('ru')}>🇷🇺 Ruso</button>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </>
