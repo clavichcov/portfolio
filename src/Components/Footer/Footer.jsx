b2f3365c75f6f21ee53cfc1d2c113edaf533ae81
Alexander Artemiev <alexanderma1983@gmail.com>
2026-03-15 11:06:51 -0500

    Actualizacion presentacion del portafolio

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css';
+import {IMAGES} from '../../Utils/Constants.js'
 
 export function Footer() {
 
@@ -6,7 +7,29 @@ export function Footer() {
     return(
         <>
             <div className='footer__content'>
-
+                <div className='footer__content_contact'>
+                    <h3 className='footer__contact_title'>
+                        Contacto:
+                    </h3>
+                    <p className='footer__contact_text'>
+                        Email:
+                    </p>
+                    <p className='footer__contact_text'>
+                        alexanderma1983@gmail.com
+                    </p>
+                    <p className='footer__contact_text'>
+                        WhatsApp: +57 320 963 7937                        
+                    </p>
+                    <p className='footer__contact_text'>
+                        Llamadas: +57 322 953 4786
+                    </p>
+                </div>
+                <div className='footer__content_social'>
+                    <a className='footer__social_link' href=""><img className='footer__social_icon' src={IMAGES.linkedin} alt="Linkedin" /></a>
+                    <a className='footer__social_link' href=""><img className='footer__social_icon' src={IMAGES.github} alt="Github" /></a>
+                    <a className='footer__social_link' href=""><img className='footer__social_icon' src={IMAGES.discord} alt="Discord" /></a>
+                    <a className='footer__social_link' href=""><img className='footer__social_icon' src={IMAGES.skype} alt="Skype" /></a>
+                </div>
             </div>
         </>
     )
--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,5 +1,12 @@
 import web from '../Images/web.jpg';
-
+import linkedin from '../Images/Icon/linkedin.png'
+import github from '../Images/Icon/github.png'
+import discord from '../Images/Icon/discord.png'
+import skype from '../Images/Icon/skype.png'
 export const IMAGES = {
     web,
+    linkedin,
+    github,
+    discord,
+    skype,
 }
